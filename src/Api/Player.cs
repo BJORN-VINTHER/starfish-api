e2a3efcc97353a52b920c55ffb37bf5fe9ac3762
sm1rt19 <49588728+sm1rt19@users.noreply.github.com>
2023-01-14 15:59:18 +0100

    Adding player get, post, delete endpoints. (#8)

---
+++ src/Api/Player.cs
@@ -0,0 +1,10 @@
+namespace Api
+{
+    public class Player
+    {
+        public string UserName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int Id { get; set; }
+    }
+}
