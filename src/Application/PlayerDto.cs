f465d41a94804821eec335d545d5c0ecbe06783c
Carsten PC <carstenvol@hotmail.com>
2023-01-14 21:58:44 +0100

    Added PlayerDto.

---
+++ src/Application/PlayerDto.cs
@@ -0,0 +1,30 @@
+﻿using Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class PlayerDto
+    {
+        public string UserName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        //public int Id { get; set; }
+
+        public PlayerDto()
+        {
+
+        }
+
+        public PlayerDto(Player player)
+        {
+            FirstName = player.FirstName;
+            LastName = player.LastName;
+            UserName = player.UserName;
+            //Id = player.Id;
+        }
+    }
+}
