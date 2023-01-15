506ed48fed231c583beb41ff704bf46710ac3f7e
sm1rt19 <49588728+sm1rt19@users.noreply.github.com>
2023-01-15 19:57:29 +0100

    Connect to session via sockets (#15)
    * Using SignalR to broadcast user message.
    
    * Update ChatHub.cs
    
    * Added player manager.

---
+++ src/Application/PlayerManager.cs
@@ -0,0 +1,89 @@
+﻿using Api;
+using Persistense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Application
+{
+    public interface IPlayerManager
+    {
+        public Player CreatePlayer(string firstName, string lastName, string userName);
+        public Player? GetPlayer(int id);
+        public Player? UpdatePlayer(int id, Player player);
+        public Player? DeletePlayer(int id);
+        public List<Player> GetPlayers();
+    }
+
+    public class PlayerManager : IPlayerManager
+    {
+        private readonly ApiContext apiContext;
+
+        public PlayerManager(ApiContext apiContext)
+        {
+            this.apiContext = apiContext;
+        }
+
+        private int CreateId()
+        {
+            int i = 1;
+            int id = apiContext.Players.Count() + i;
+            while (GetPlayer(id) != null)
+            {
+                id = apiContext.Players.Count() + ++i;
+            }
+            return id;
+        }
+
+        public Player CreatePlayer(string firstName, string lastName, string userName)
+        {
+            Player player = new Player();
+            player.FirstName = firstName;
+            player.LastName = lastName;
+            player.UserName = userName;
+            player.Id = CreateId();
+            apiContext.Players.Add(player);
+            apiContext.SaveChanges();
+            return player;
+        }
+
+        public Player? GetPlayer(int id)
+        {
+            return apiContext.Players.SingleOrDefault(p => p.Id == id);
+        }
+
+        public Player? UpdatePlayer(int id, Player input)
+        {
+            Player? player = GetPlayer(id);
+            if (player == null)
+            {
+                return null;
+            }
+            player.FirstName = input.FirstName;
+            player.LastName = input.LastName;
+            player.UserName = input.UserName;
+            apiContext.SaveChanges();
+            return player;
+        }
+
+        public Player? DeletePlayer(int id)
+        {
+            Player? player = GetPlayer(id);
+            if (player == null)
+            {
+                return null;
+            }
+            apiContext.Players.Remove(player);
+            apiContext.SaveChanges();
+            return player;
+        }
+
+        public List<Player> GetPlayers()
+        {
+            return apiContext.Players.ToList();
+        }
+    }
+}
